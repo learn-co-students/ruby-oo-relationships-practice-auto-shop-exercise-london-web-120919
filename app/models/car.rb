f1a3a602b32709a8be24b20c215f9d023fbd3cd5
flatironschool <t.benson@live.co.uk>
2019-12-15 19:05:47 +0000

    Done.

--- app/models/car.rb
+++ app/models/car.rb
@@ -1,11 +1,30 @@
 class Car
 
-  attr_reader :make, :model
+  attr_accessor :owner, :mechanic
+  attr_reader :make, :model, :classification
 
-  def initialize(make, model, classification)
+  @@all = []
+
+  def initialize(make:, model:, classification:, owner: nil)
     @make = make
     @model = model
     @classification = classification
+    @owner = owner
+    @@all << self
   end
+
+  def self.all
+    @@all
+  end
+
+  def self.classifications
+    self.all.map { |car| car.classification }.uniq
+  end
+
+  def mechanics
+    Mechanic.all.select { |person| person.specialty.downcase == self.classification.downcase }
+  end
+
+
 
 end
--- tools/console.rb
+++ tools/console.rb
@@ -4,4 +4,56 @@ def reload
   load 'config/environment.rb'
 end
 
+toli = CarOwner.new("Toli")
+tim = CarOwner.new("Tim")
+mabel = CarOwner.new("Mabel")
+# fern  = CarOwner.new("Fern")
+
+puts "CarOwner.all"
+puts CarOwner.all
+
+bob = Mechanic.new(name: "Bob", specialty: "sport")
+dave = Mechanic.new(name: "Dave", specialty: "Vintage")
+john = Mechanic.new(name: "John", specialty: "Family")
+fran = Mechanic.new(name: "Fran", specialty: "Van")
+
+puts "Mechanic.all"
+puts Mechanic.all
+
+transit = Car.new(make: "Ford", model: "Transit", classification: "Van")
+transit.owner = toli
+
+aclass = Car.new(make: "Mercedes", model: "A-class", classification: "sport", owner: tim)
+beetle = Car.new(make: "Volkswagen", model: "Beetle", classification: "vintage", owner: tim)
+mini = Car.new(make: "Mini", model: "original", classification: "vintage", owner: tim)
+picasso = Car.new(make: "renaut", model: "picasso", classification: "family", owner: mabel)
+a5 = Car.new(make: "Audi", model: "A5", classification: "sport", owner: toli)
+
+
+puts "all cars"
+puts Car.all
+
+puts "Tim's mechanics"
+puts tim.my_mechanics
+
+puts "bob's cars"
+puts bob.my_cars
+
+puts "bob's owners"
+puts bob.car_owners
+
+puts "bob's owners' names"
+puts bob.car_owners_by_name
+
+puts "all car classifications"
+puts Car.classifications
+
+puts "mini mechanics"
+puts mini.mechanics
+
+puts "a5 mechanics"
+puts a5.mechanics
+
+
+
 binding.pry
