9df4a69d1cb827896c3e32b6cd7e99d74030c7ae
Mariola Pasko <mariolapasko@Mariolas-MacBook-Air.local>
2019-12-16 14:19:53 +0000

    Done.

--- app/models/car.rb
+++ app/models/car.rb
@@ -1,11 +1,26 @@
 class Car
 
-  attr_reader :make, :model
+  attr_reader :make, :model, :classification, :carowner, :mechanic
 
-  def initialize(make, model, classification)
+  @@all = []
+
+  def initialize(make, model, classification, carowner, mechanic)
     @make = make
     @model = model
     @classification = classification
+    @carowner = carowner
+    @mechanic = mechanic
+    @@all << self 
   end
+
+  def self.all #Get a list of all cars
+    @@all
+  end 
+  
+  #Get a list of mechanics that have an expertise that matches the car classification
+  def mechanics
+    #car.classification == mechanic.specialty
+    Mechanic.all.select{|mechanic| mechanic.specialty == self.classification}
+  end 
 
 end
--- tools/console.rb
+++ tools/console.rb
@@ -4,4 +4,18 @@ def reload
   load 'config/environment.rb'
 end
 
+carowner1 = CarOwner.new("Bob")
+carowner2 = CarOwner.new("James")
+carowner3 = CarOwner.new("Ben")
+
+mechanic1 = Mechanic.new("Mike", "antique")
+mechanic2 = Mechanic.new("Si", "exotic")
+mechanic3 = Mechanic.new("Dave", "clunker")
+
+car1 = Car.new("make1", "old", "antique", carowner1, mechanic1)
+car2 = Car.new("make2", "new", "exotic", carowner2, mechanic2)
+car3 = Car.new("make3", "new", "clunker", carowner1, mechanic1)
+car4 = Car.new("make4", "old", "antique", carowner1, mechanic1)
+car5 = Car.new("make4", "new", "clunker", carowner3, mechanic3)
+
 binding.pry
